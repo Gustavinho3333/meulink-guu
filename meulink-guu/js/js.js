cbf5c5a712f18f9bf486dafa58d1201b539100b3
Gustavinho3333 <gustavosilvagomes802@gmail.com>
2025-11-03 21:02:33 -0300

    Delete meulink-guu directory

--- meulink-guu/js/js.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById('toggle-theme');
-button.addEventListener('click', () => {
-  document.body.classList.toggle('dark');
-  const mode = document.body.classList.contains('dark') ? '🌙' : '🌞';
-  button.textContent = mode;
-});
